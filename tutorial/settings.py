a70ac0e28828b9907e1d3a035daf481ce2146c13
Adil Assouab <assouab@atrihub.io>
2020-04-02 12:58:35 -0700

    feat(REST Tutorial): DB setup and create Snippet model
    Changed database settings.
    Added Snippet model.
    Added initial migration to Snippet model.
    
    [ICTRAINING-386]

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aa_resttutorial',
+        'USER': 'postgres',
+        'PASSWORD': 'helloworld'
     }
 }
 
